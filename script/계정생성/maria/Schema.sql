e42454c4686472a9f616a4d245c60a56ff0ac0ca
PhilSang Son <niceguyths@naver.com>
2023-06-15 17:09:18 +0900

    Update Schema.sql

--- script/계정생성/maria/Schema.sql
+++ script/계정생성/maria/Schema.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE IF NOT EXISTS `pss_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
-CREATE USER 'pss'@'localhost' IDENTIFIED BY 'thsvlftkddjrakswkdwk0!029!6!526';
-CREATE USER 'pss'@'%' IDENTIFIED BY 'thsvlftkddjrakswkdwk0!029!6!526';
+CREATE DATABASE IF NOT EXISTS `` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+CREATE USER 'pss'@'localhost' IDENTIFIED BY '';
+CREATE USER 'pss'@'%' IDENTIFIED BY '';
 
 GRANT ALL ON pss_db.* TO 'pss'@'localhost';
 GRANT ALL ON pss_db.* TO 'pss'@'%';
